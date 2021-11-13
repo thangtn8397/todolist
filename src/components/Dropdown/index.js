5d9a8a03b22999806269535f52d202fe797d8db3
ThangTN <thangtn8397@gmail.com>
2021-11-13 11:22:39 +0700

    add common component Dropdown

---
+++ src/components/Dropdown/index.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import styles from "./Dropdown.module.css";
+import ArrowDropDownIcon from "@mui/icons-material/ArrowDropDown";
+
+const Dropdown = ({ label }) => {
+  const [priority, setPriority] = useState(priorityArray[0]);
+  const [showPopup, setShowPopup] = useState(false);
+  let popupStyleArray = !showPopup
+    ? [styles.Popup, styles.Hidden]
+    : [styles.Popup];
+  let popupStyle = popupStyleArray.join(" ");
+
+  const onClickItem = (priority) => {
+    setPriority(priority);
+    setShowPopup(false);
+  };
+
+  return (
+    <div className={styles.DropdownContainer}>
+      <label className={styles.Label}>Priority</label>
+      <div className={styles.Dropdown} onClick={() => setShowPopup(!showPopup)}>
+        <div>{priority.label}</div>
+        <ArrowDropDownIcon />
+      </div>
+      <ul className={popupStyle}>
+        {priorityArray.map((p) => (
+          <li
+            onClick={() => onClickItem(p)}
+            key={p.value}
+            className={p.value === priority.value ? styles.Selected : ""}
+          >
+            {p.label}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Dropdown;
+
+const priorityArray = [
+  { value: "normal", label: "Normal" },
+  { value: "high", label: "High" },
+  { value: "low", label: "Low" },
+];
--- src/components/TaskForm/index.js
+++ src/components/TaskForm/index.js
@@ -1,16 +1,17 @@
 import React from "react";
 import Button from "../Button";
+import Dropdown from "../Dropdown";
 import Input from "../Input";
 import styles from "./TaskForm.module.css";
 
 const TaskForm = () => {
   return (
     <div>
-      <Input label="add task" placeholder="Add new task" inputType="text" />
+      <Input placeholder="Add new task..." inputType="text" />
       <Input inputType="textarea" label="Description" />
       <div className={styles.Flex}>
         <Input type="date" label="Due date" />
-        <Input label="Piority" />
+        <Dropdown label="Piority" />
       </div>
       <Button btnType="success" value="Add" className={styles.BtnAdd} />
     </div>
